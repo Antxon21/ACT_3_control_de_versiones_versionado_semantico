eaab7f83224f0c3eb421ad3e5cb90cb43a51a40b
Antxon21 <amocotorralba@iessonferrer.net>
2024-01-16 19:08:35 +0100

    Creación de la clase y asignación de atributos

---
+++ IdeaProjects/Practica 4 ED/src/Coche.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * En este punto creamos la clase "Coche" y definimos sus atributos
+ * Los atributos son los simples de "Coche": Matricula, Modelo, Caballos
+ * @author Antxón Moço / IES Son Ferrer
+ * @version 1.1.0
+ */
+
+public class Coche {
+//ATRIBUTOS
+
+    private String Matricula;
+    private String Modelo;
+    private Integer Caballos;
+
+//GET
+
+}
